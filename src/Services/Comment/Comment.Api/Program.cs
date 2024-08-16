b9d05df8f5e6f572a6c8eb648b33e927385ddb62
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-08-16 23:45:05 +0330

    Add project files.

---
+++ src/Services/Comment/Comment.Api/Program.cs
@@ -0,0 +1,26 @@
+namespace Comment.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
