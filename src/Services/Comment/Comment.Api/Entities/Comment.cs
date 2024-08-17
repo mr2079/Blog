ecef20da630eef5a3949359075f4876f5ef727ed
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-08-17 21:02:26 +0330

    Add comment service entities

---
+++ src/Services/Comment/Comment.Api/Entities/Comment.cs
@@ -0,0 +1,79 @@
+﻿using MongoDB.Bson;
+
+namespace Comment.Api.Entities;
+
+public sealed class Comment : Entity
+{
+    private Comment(
+        ObjectId id,
+        string userId,
+        string articleId,
+        string text) : base(id)
+    {
+        UserId = userId;
+        ArticleId = articleId;
+        Text = text;
+    }
+
+    public string UserId { get; set; }
+    public string ArticleId { get; set; }
+    public string Text { get; set; }
+    public ICollection<Comment>? Replies { get; set; }
+
+    public static Comment Create(
+        ObjectId id,
+        object userId,
+        object articleId,
+        string text)
+    {
+        return new Comment(
+            id,
+            userId.ToString()!,
+            articleId.ToString()!,
+            text);
+    }
+
+    public void Update(
+        string text)
+    {
+        Text = text;
+    }
+
+    public void AddReply(Comment reply)
+    {
+        var replies = Replies?.ToList() ?? new List<Comment>();
+        replies.Add(reply);
+        Replies = replies;
+    }
+
+    public void AddReply(params Comment[] reply)
+    {
+        var replies = Replies?.ToList() ?? new List<Comment>();
+        replies.AddRange(reply);
+        Replies = replies;
+    }
+
+    public void AddReply(IEnumerable<Comment> reply)
+    {
+        var replies = Replies?.ToList() ?? new List<Comment>();
+        replies.AddRange(reply);
+        Replies = replies;
+    }
+
+    public void RemoveReply(Comment reply)
+    {
+        var replies = Replies?.ToList();
+        replies?.Remove(reply);
+        Replies = replies;
+    }
+
+    public void RemoveReply(ObjectId id)
+    {
+        var replies = Replies?.ToList();
+        var reply = replies?.SingleOrDefault(c => Equals(c.Id, id));
+        if (reply is not null)
+        {
+            RemoveReply(reply);
+        } 
+    }
+}
---
+++ src/Services/Comment/Comment.Api/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Comment.Api.Entities;
+
+public abstract class Entity(ObjectId id)
+{
+    public ObjectId Id { get; set; } = id;
+
+    [BsonElement("created_at")]
+    [BsonRepresentation(BsonType.Timestamp)]
+    public DateTime CreatedAt { get; set; }
+
+    [BsonElement("updated_at")]
+    [BsonRepresentation(BsonType.Timestamp)]
+    public DateTime UpdatedAt { get; set; }
+
+    [BsonElement("is_accepted")]
+    public bool IsAccepted { get; set; }
+
+    [BsonElement("is_deleted")]
+    public bool IsDeleted { get; set; }
+}
