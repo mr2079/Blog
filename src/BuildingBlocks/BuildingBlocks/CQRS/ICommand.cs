d7c96ac3db038d0f7c4121818582cde2149aeb5d
mr2079 <mohammadalikhoshdooni@yahoo.com>
2024-08-18 11:38:53 +0330

    Add building blocks for CQRS

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>;
+
+public interface ICommand<out TResponse>
+    : IRequest<TResponse>
+    where TResponse : notnull;
