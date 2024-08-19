10514094de0ae10832d585399b157ddbf5aa1989
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-08-19 21:01:05 +0330

    Add get comments list

--- src/Services/Comment/Comment.Api/Endpoints/GetComment/GetCommentEndpoint.cs
+++ src/Services/Comment/Comment.Api/Endpoints/GetComment/GetCommentEndpoint.cs
@@ -6,12 +6,6 @@ using MongoDB.Bson;
 
 namespace Comment.Api.Endpoints.GetComment;
 
-public record GetCommentListRequest(
-    string? UserId,
-    string? ArticleId,
-    int? Skip,
-    int? Limit);
-
 public record GetCommentListResponse();
 
 public record GetCommentResponse();
@@ -20,20 +14,27 @@ public class GetCommentEndpoint() : CarterModule("api/comment")
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        //app.MapGet("/", async Task<IResult> (
-        //    GetCommentListRequest request,
-        //    IMapper mapper,
-        //    ISender sender,
-        //    CancellationToken cancellationToken) =>
-        //{
-        //    var query = mapper.Map<GetCommentListQuery>(request);
+        app.MapGet("/", async Task<IResult> (
+            [FromQuery] string? userId,
+            [FromQuery] string? articleId,
+            [FromQuery] int? skip,
+            [FromQuery] int? limit,
+            IMapper mapper,
+            ISender sender,
+            CancellationToken cancellationToken) =>
+        {
+            var query = new GetCommentListQuery(
+                userId,
+                articleId,
+                skip,
+                limit);
 
-        //    var result = await sender.Send(query, cancellationToken);
+            var result = await sender.Send(query, cancellationToken);
 
-        //    var response = mapper.Map<GetCommentListResponse>(result);
+            var response = mapper.Map<GetCommentListResponse>(result);
 
-        //    return Results.Ok(response);
-        //});
+            return Results.Ok(response);
+        });
 
         app.MapGet("/{id}", async Task<IResult> (
             [FromRoute] ObjectId id,
