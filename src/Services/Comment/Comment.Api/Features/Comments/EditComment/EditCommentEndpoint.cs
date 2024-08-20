a1ac792408b3742440ea7c4965204409556082f3
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-08-20 21:29:20 +0330

    Fix edit endpoint route param

--- src/Services/Comment/Comment.Api/Features/Comments/EditComment/EditCommentEndpoint.cs
+++ src/Services/Comment/Comment.Api/Features/Comments/EditComment/EditCommentEndpoint.cs
@@ -15,12 +15,12 @@ public class DeleteCommentEndpoint() : CarterModule("api/comment")
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapPut("edit/{id}", async Task<IResult> (
-            [FromRoute] ObjectId id,
+            [FromRoute] string id,
             [FromBody] EditCommentRequest request,
             ISender sender,
             CancellationToken cancellationToken) =>
         {
-            var command = new EditCommentCommand(id, request.Text);
+            var command = new EditCommentCommand(ObjectId.Parse(id), request.Text);
 
             var result = await sender.Send(command, cancellationToken);
 
